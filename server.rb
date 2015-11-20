56009c265c94375c54a9cd972fbf771e7567f7eb
Jonathan Devers <jonathan.devers@gmail.com>
2015-11-19 21:28:02 -0500

    fix resubmit error

--- server.rb
+++ server.rb
@@ -1,33 +1,30 @@
 require "sinatra"
+require "pry"
 require_relative 'models/rps'
 
 use Rack::Session::Cookie, {
   secret: "keep_it_secret_keep_it_safe"
 }
 
-
-
-rps_game = RPS.new
-
-
-
 get '/' do
-  session[:rounds] = 0
-  # session[:visit_count].nil? ? session[:visit_count] = 1 : session[:visit_count] += 1
-  session[:rps_game] = rps_game
+  if session[:rounds].nil?
+    session[:rounds] = 0
+  end
+
+  if session[:rps_game].nil?
+    session[:rps_game] = RPS.new
+  end
   erb :game
 end
 
 post '/choose' do
-  session[:rps_game]= session[:rps_game]
-  session[:choice] = params[:choice]
+  choice = params[:choice]
+  session[:outcome] = session[:rps_game].round_outcome(choice)
   session[:rounds] += 1
-  # rps_game.evaluate(@choice)
-  # binding.pry
-  erb :game
+  redirect '/'
 end
 
-post '/reset' do
+get '/reset' do
   session.clear
   redirect '/'
 end
